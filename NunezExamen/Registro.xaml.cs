dec6b1ae9e9b1a55e5e44125c5f6d91d4b1d4190
PC <PC@DESKTOP-GF3EBS0>
2023-01-11 00:42:49 -0500

    Agregar archivos de proyecto.

---
+++ NunezExamen/Registro.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NunezExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(String usuario)
+        {
+            InitializeComponent();
+            lblUsuario.Text = usuario;
+        }
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Convert.ToDouble(txtMonto.Text) < 1)
+                {
+                    DisplayAlert("Error", "Ingrese un monto mayor o igual 1", "Cerrar");
+                }
+                if (Convert.ToDouble(txtMonto.Text) > 4000)
+                {
+                    DisplayAlert("Error", "Inngrese un monto menor o igual a 4000", "Cerrar");
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            try
+            {
+                double montoInicial = Convert.ToDouble(txtMonto.Text);
+                double Pago = ((4000 - montoInicial) / 5) +(4000 * 0.05);
+                double pagoTotal = Pago * 5 + montoInicial;
+                
+                txtPago.Text = Pago.ToString();
+                txtPagoTotal.Text = pagoTotal.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                DisplayAlert("Alerta", ex.ToString(), "Cerrar");
+
+            }
+
+
+        }
+
+        private void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Resumen(lblUsuario.Text, txtNombre.Text, txtPago.Text));
+        }
+    }
+}
---
+++ NunezExamen/Resumen.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NunezExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Resumen : ContentPage
+    {
+        public Resumen(string usuario, string nombre, string total)
+        {
+            InitializeComponent();
+            lblUsuario.Text = usuario;
+            txtNombre.Text = nombre;
+            txtTotal.Text = total;
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAcerca_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Examen resuelto por: ", "PAUL ALEXANDER NUÑEZ PARCO", "ACEPTAR");
+        }
+
+    }
+}
